4732305a47903e45f7e2b9eaecf82464cd81c1bf
mustafa20170986 <jster0986@gmail.com>
2026-07-19 12:00:00 +0600

    updated layout with clerk

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,24 +2,34 @@ import Navbar from "@/components/navbar";
 import "./globals.css";
 import Button from "@/components/button";
 import Acc from "@/components/according";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import Drawer from "@/components/drawer";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>)
+
+{
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className="min-h-full flex flex-col">
-        <Navbar />
+    
+       
     
       
-        <main className="flex-1">{children}
+        <main className="flex-1">
+       
+  {children}
+        
+       
               <div className="flex">
-  <Acc />
+
         </div>
         </main>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
