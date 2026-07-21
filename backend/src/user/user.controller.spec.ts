4836c7c2df7c10c4e7403a8771b65781fb4156e1
mustafa20170986 <jster0986@gmail.com>
2026-07-21 12:00:00 +0600

    test file fixed for user

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -4,13 +4,9 @@ import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
-  //we took the userService
   let service: UserService;
 
-  //mock prisma function create
   const mockService = {
-    //its the ethod in the servivce part
-    // which is responsible for creating new user
     createuser: jest.fn(),
   };
 
@@ -28,6 +24,7 @@ describe('UserController', () => {
     controller = module.get<UserController>(UserController);
     service = module.get<UserService>(UserService);
   });
+
   beforeEach(() => {
     jest.clearAllMocks();
   });
@@ -36,25 +33,29 @@ describe('UserController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('it should run and called the body paylaod with parameter', async () => {
-    const body = { name: 'suborna', email: 'suborna2025@gmail.com' };
-    const mockrespone: User = {
+  it('should call createuser with id, name and email', async () => {
+    const body = {
       id: '8210uehfei',
       name: 'suborna',
       email: 'suborna2025@gmail.com',
     };
-    //execute the method
-    mockService.createuser.mockResolvedValue(mockrespone);
+
+    const mockResponse = {
+      id: '8210uehfei',
+      name: 'suborna',
+      email: 'suborna2025@gmail.com',
+    };
+
+    mockService.createuser.mockResolvedValue(mockResponse);
+
     const result = await controller.createuser(body);
-    //expect the calls
-    //we expect the create user to called with our input data
+
     expect(service.createuser).toHaveBeenCalledWith(
+      '8210uehfei',
       'suborna',
       'suborna2025@gmail.com',
     );
-    //expect to have calle dthis exact one
     expect(service.createuser).toHaveBeenCalledTimes(1);
-    // expect to get the response equal to mock response
-    expect(result).toEqual(mockrespone);
+    expect(result).toEqual(mockResponse);
   });
 });
