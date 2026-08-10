0b1ba0a58d05ace0e1199db36a78d3a259df6cdd
mustafa20170986 <jster0986@gmail.com>
2026-08-10 12:00:00 +0600

    final main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { clerkMiddleware } from '@clerk/express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  app.use(clerkMiddleware());
+  await app.listen(process.env.PORT ?? 2017);
 }
 bootstrap();
