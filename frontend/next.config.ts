d91503ac7c1b7e994ac60486cf7d31760f7c0586
mustafa20170986 <jster0986@gmail.com>
2026-08-22 12:00:00 +0600

    next config std alone

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
+        protocol: "https",
+        hostname: "plus.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
